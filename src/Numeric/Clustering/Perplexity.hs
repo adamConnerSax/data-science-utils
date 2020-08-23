4d351f5d4872f4feb223d54e9f02eb4f91fcb7c9
adamConnerSax <adam_conner_sax@yahoo.com>
2020-08-22 21:15:53 -0400

    Fixed x * log x perplexity bug

--- src/Numeric/Clustering/Perplexity.hs
+++ src/Numeric/Clustering/Perplexity.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Numeric.Clustering.Perplexity where
 
 import qualified Data.Massiv.Array as MA
@@ -14,31 +15,43 @@ pGivenBeta rowIndex distances beta =
       sumRaw = MA.sum raw
   in raw MA..* (1 / sumRaw)
 
-entropy :: MA.Source r MA.Ix1 Double => MA.Vector r Double -> MA.Ix1 -> Double -> Double
-entropy distances rowIndex beta = MA.sum $ MA.map (\x -> -x * log x) $ pGivenBeta rowIndex distances beta 
+safe_xLogx :: Double -> Double
+safe_xLogx x = if x > 1e-7 then x * log x else 0
 
-findBeta :: MA.Source r MA.Ix1 Double => Double -> MA.Ix1 -> MA.Vector r Double -> Either NLOPT.Result NLOPT.Solution
+entropy :: MA.Source r MA.Ix1 Double => MA.Vector r Double -> MA.Ix1 -> Double -> Double
+entropy distances rowIndex beta = MA.sum $ MA.map (\x -> negate $ safe_xLogx x) $ pGivenBeta rowIndex distances beta 
+
+findBeta :: MA.Source r MA.Ix1 Double => Double -> MA.Ix1 -> MA.Vector r Double -> Either Text.Text Double
 findBeta perplexity rowIndex distances =
   let targetEntropy = log perplexity
-      obj x = targetEntropy - entropy distances rowIndex (x `LA.atIndex` 0)
+      obj x = abs (targetEntropy - entropy distances rowIndex (x `LA.atIndex` 0))
       algo = NLOPT.NELDERMEAD obj [] Nothing
       stopWhenAny = NLOPT.ObjectiveAbsoluteTolerance 1e-4 NLOPT.:|
                     [NLOPT.MaximumEvaluations 50
                     ]
       problem = NLOPT.LocalProblem 1 stopWhenAny algo
-  in NLOPT.minimizeLocal problem (LA.fromList [1])
+      optimizedE = NLOPT.minimizeLocal problem (LA.fromList [1])
+      textError sol = (Text.pack $ show $ NLOPT.solutionResult sol)
+                      <> ": x = "
+                      <> (Text.pack $ show $ NLOPT.solutionParams sol)
+                      <> "; obj(x) = "
+                      <> (Text.pack $ show $ NLOPT.solutionCost sol)
+  in case optimizedE of
+    Left r -> Left $ Text.pack $ show r
+    Right s -> case NLOPT.solutionResult s of
+      NLOPT.FTOL_REACHED -> Right $ (`LA.atIndex` 0) $ NLOPT.solutionParams s
+      _ -> Left $ textError s
   
-
 data SolveException = SolveException Text.Text deriving (Show)
 instance X.Exception SolveException
      
 probabilities :: MA.MonadThrow m => Double -> MA.Matrix MA.U Double -> m (MA.Matrix MA.U Double)
 probabilities perplexity distances = do
   let distanceRows = MA.computeAs MA.B $ MA.outerSlices distances
-      betaSolsE :: Either NLOPT.Result (MA.Vector MA.U Double) 
-      betaSolsE = MA.itraverseA (\ix v -> fmap ((`LA.atIndex` 0) . NLOPT.solutionParams) $ findBeta perplexity ix v) distanceRows
+      betaSolsE :: Either Text.Text (MA.Vector MA.U Double) 
+      betaSolsE = MA.itraverseA (\ix v -> findBeta perplexity ix v) distanceRows   
   betas <- case betaSolsE of
-    Left res -> X.throwM (SolveException $ Text.pack $ show res)
-    Right x -> return x
+    Left err -> X.throwM (SolveException err)
+    Right x -> return x    
   fmap MA.compute $ MA.stackOuterSlicesM $ MA.izipWith pGivenBeta distanceRows betas
   
