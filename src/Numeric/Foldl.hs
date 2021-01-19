b364ec3ca3d3ef4dfe809550b200823d4edd3026
adamConnerSax <adam_conner_sax@yahoo.com>
2021-01-19 15:47:12 -0500

    Made median fold stricter

--- src/Numeric/Foldl.hs
+++ src/Numeric/Foldl.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Numeric.Foldl where
 
@@ -14,18 +15,22 @@ wgtdSumF :: (RealFrac b, RealFrac c)
 wgtdSumF wgt sumOf = (/) <$> sumWithWeightsF wgt sumOf <*> fmap realToFrac (FL.premap wgt FL.sum)
 {-# INLINE wgtdSumF #-}
 
-weightedMedian :: forall a.(Ord a) => a -> [(Double, a)] -> a
-weightedMedian dfltA l =
-  let ordered :: [(Double, a)] = L.sortOn snd l
-      middleWeight :: Double = FL.fold (FL.premap fst FL.sum) l / 2
-      update :: (Double, a) -> (Double, a) -> ((Double, a), ())
-      update (wgtSoFar, medianSoFar) (w, x) = ((wgtSoFar + w, newMedian), ()) where
+data StrictPair a = StrictPair !Double !a
+getFst (StrictPair x _) = x
+getSnd (StrictPair _ a) = a
+
+weightedMedian :: forall a.(Ord a) => a -> [StrictPair a] -> a
+weightedMedian dfltA !l =
+  let ordered :: [StrictPair a] = L.sortOn getSnd l
+      middleWeight :: Double = FL.fold (FL.premap getFst FL.sum) l / 2
+      update :: StrictPair a -> StrictPair a -> (StrictPair a, ())
+      update (StrictPair wgtSoFar medianSoFar) (StrictPair w x) = (StrictPair (wgtSoFar + w) newMedian, ()) where
         newMedian = if wgtSoFar <= middleWeight then x else medianSoFar
       -- mapAccumL :: Traversable t => (a -> b -> (a, c)) -> a -> t b -> (a, t c)
-      ((_, res), _) = L.mapAccumL update (0, dfltA) ordered
+      (StrictPair _ res, _) = L.mapAccumL update (StrictPair 0 dfltA) ordered
   in res
 {-# INLINE weightedMedian #-}
 
 weightedMedianF :: (Num b, Ord b) => (a -> Double) -> (a -> b) -> FL.Fold a b
-weightedMedianF wgt median = weightedMedian 0 <$> FL.premap (wgt &&& median) FL.list
+weightedMedianF wgt median = weightedMedian 0 <$> FL.premap (\x -> StrictPair (wgt x) (median x)) FL.list
 {-# INLINE weightedMedianF #-}
